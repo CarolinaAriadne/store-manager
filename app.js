c6432cd2b29ab02bc30a2408a9c11e08cf6bda71
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-05-06 19:23:36 -0300

    Alteraçao arquivo app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const router = require('./routes/router');
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
