ac2fa6473e9856c8917f4219a9a6ca0d603cf505
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-05-07 12:00:09 -0300

    Produto procurado pelo id, n retornando no insomnia ainda - request infinita >> F, func getByIdServiceProduct feita, controllers modificados

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,13 +1,19 @@
 const productsService = require('../services/productsService');
 
-const getAllControllerProducts = async (_req, res) => {
-    const productsAll = await productsService.getAllServiceProducts();
+const getAllControllerProducts = async (_req, res, next) => {
+    try {
+        const productsAll = await productsService.getAllServiceProducts();
 
-    return res.status(200).json(productsAll);
+        return res.status(200).json(productsAll);
+    } catch (error) {
+        next(error);
+    }
 };
 
-const getByIdControllerProducts = () => {
-    
+const getByIdControllerProducts = async (req, res) => {
+    const { id } = req.params;
+    const productId = await productsService.getByIdServiceProduct(id);
+        return res.status(200).json(productId);
 };
 
 module.exports = {
--- services/productsService.js
+++ services/productsService.js
@@ -1,11 +1,28 @@
 const productsModel = require('../models/productsModel');
 
+const erroHandler = (status, message) => ({
+    status,
+    message,
+});
+
 const getAllServiceProducts = async () => {
     const productsAll = await productsModel.getAllProductsModel();
-
+    // console.log(productsAll);
+    if (productsAll.length === 0) {
+        throw erroHandler(404, 'Products not returned ');
+    }
     return productsAll;
+};
+
+const getByIdServiceProduct = async (id) => {
+    const productById = await productsModel.getByIdProductsModel(id);
+    if (productById.length === 0) {
+        throw erroHandler(404, 'Products not returned ');
+    }
+    return productById;
 };
 
 module.exports = {
     getAllServiceProducts,
+    getByIdServiceProduct,
 };
