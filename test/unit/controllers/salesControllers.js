37b6c2cdd9058e6221207ff6baf437e173bdf133
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-05-13 10:26:42 -0300

    Requisito e pra passar agora, em tese, oremos

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -42,5 +42,66 @@ describe('Testa retorno das vendas', () => {
     })
 })
 describe('Testa se a busca pelo id retorna a venda', () => {
-    
+    describe('Produto encontrado pelo id', () => {
+
+        const resultExequest = {
+            id: 1,
+            name: "Caixa de pandora",
+            quantity: 1,
+            date: "2022-05-03 08:13:12"
+          }
+
+        const request = {};
+        const response = {}
+
+        before(() => {
+            request.params = {id: 1}
+            response.status = sinon.stub().returns(response);
+            response.json = sinon.stub().returns();
+
+            sinon.stub(salesService, 'getByIdServiceSales').resolves(resultExequest)
+        })
+
+        after(() => {
+            salesService.getByIdServiceSales.restore()
+        })
+        it('É retornando o  status 200', async () => {
+            await salesControler.getByIdControllerSales(request, response)
+
+            expect(response.status.calledWith(200)).to.be.equal(true)
+        })
+        it('É retornando um objeto', async () => {
+            await salesControler.getByIdControllerSales(request, response)
+
+            expect(response.json.calledWith(sinon.match.object)).to.be.equal(true)
+        })
+
+
+
+    })
+    describe('Produto não encontrado', () => {
+        const request = {};
+        const response = {}
+        const err = { status: 404, message: 'Sale not found'}
+
+        before(() => {
+            req.params = {id: 45}
+            response.status = sinon.stub().returns(response);
+            response.json = sinon.stub().returns();
+
+            sinon.stub(salesService, 'getByIdServiceSales').resolves(err)
+        })
+
+        after(() => {
+            salesService.getByIdServiceSales.restore()
+            it('Retorna a mensagem "Sale not found"', async () => {
+                try {
+                    await salesControler.getByIdControllerSales(request, response)
+                } catch {
+                    expect(err.message).to.be.equal('Sale not found')
+                }
+            })
+    })
+})
+
 })
