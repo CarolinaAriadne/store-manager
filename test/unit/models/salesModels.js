2288c2fb4678ee087f782a133259f41d66bcf715
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-05-12 22:14:56 -0300

    Req q incompleto, getAllServiceSales camada service ok

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -1,11 +1,11 @@
 const { expect } = require('chai');
-const salesService = require('./../../../models/salesModel');
+const salesModel = require('./../../../models/salesModel');
 const connection = require('./../../../models/connection');
 const { response } = require('express');
 const sinon = require('sinon');
 const { execute } = require('./../../../models/connection');
 
-describe.only('Busca todas as vendas no BD, func getAllSalesModel', () => {
+describe('Busca todas as vendas no BD, func getAllSalesModel', () => {
     describe('Retorna as vendas', () => {
         const executeResponse = [{
             saleId: 1,
@@ -22,12 +22,12 @@ describe.only('Busca todas as vendas no BD, func getAllSalesModel', () => {
             connection.execute.restore()
         })
         it('É retornado um array', async () => {
-            const result = await salesService.getAllSalesModel()
+            const result = await salesModele.getAllSalesModel()
 
             expect(result).to.be.an('array')
         })
         it('Objetos do array contém os atributos saleId, date, productId e quantity', async () => {
-            const [result] = await salesService.getAllSalesModel()
+            const [result] = await salesModel.getAllSalesModel()
             expect(result).to.be.includes.all.keys(
                 'saleId',
                 'date',
@@ -47,7 +47,7 @@ describe.only('Busca todas as vendas no BD, func getAllSalesModel', () => {
             connection.execute.restore()
         })
         it('Espera um array vazio quando vendas não são encontradas', async () => {
-            const result = await salesService.getAllSalesModel()
+            const result = await salesModel.getAllSalesModel()
 
             expect(result).to.be.empty;
         })
---
+++ test/unit/services/salesServices.js
@@ -0,0 +1,56 @@
+const { expect } = require('chai');
+const salesService = require('./../../../services/salesService')
+const salesModel = require('./../../../models/salesModel');
+const connection = require('./../../../models/connection');
+const { response } = require('express');
+const sinon = require('sinon');
+const { execute } = require('./../../../models/connection');
+
+describe.only('Busca todas as vendas no BD, func getAllServiceSales', () => {
+    describe('Retorna as vendas', () => {
+        const executeResponse = [{
+            saleId: 1,
+            date: '2022-05-12T00:46:00.000Z',
+            productId: 1,
+            quantity: 5
+        },
+        ]
+
+        before(() => {
+            sinon.stub(salesModel, 'getAllSalesModel').resolves(executeResponse)
+        })
+        after(() => {
+            salesModel.getAllSalesModel.restore()
+        })
+        it('É retornado um array', async () => {
+            const result = await salesService.getAllServiceSales()
+
+            expect(result).to.be.an('array')
+        })
+        it('Objetos do array contém os atributos saleId, date, productId e quantity', async () => {
+            const [result] = await salesService.getAllServiceSales()
+            expect(result).to.be.includes.all.keys(
+                'saleId',
+                'date',
+                'productId',
+                'quantity'
+            )    
+        });
+
+    })
+    describe('Quando não são retornadas as vendas', () => {
+        const executeResponse = []
+
+        before(() => {
+            sinon.stub(salesModel, 'getAllSalesModel').resolves([executeResponse])
+        })
+        after(() => {
+            salesModel.getAllSalesModel.restore();
+        })
+        it('Espera um array vazio quando vendas não são encontradas', async () => {
+            const [result] = await salesService.getAllServiceSales()
+
+            expect(result).to.be.empty;
+        })
+    })
+})
